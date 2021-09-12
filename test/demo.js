5d4921036a818ef6040b41bd9b2540919c40fbf7
Shuyi (Suzy) Wang <90553869+less-cat@users.noreply.github.com>
2021-09-12 10:21:25 +0000

    Create demo.js

---
+++ test/demo.js
@@ -0,0 +1,11 @@
+avatar-upload-container
+dropdown-item
+
+avatarBtn.click(function(e){ console.log( e ) } );
+$(document).ready(function () {
+    setTimeout(function (){
+        var avatarBtn = 
+        console.log(avatarBtn);
+        avatarBtn.click();
+    }, 3000);
+});
